fc3933bc0fa7eb548c91d66374f08b6b148b7e3d
Jochen Topf <jochen@topf.org>
2019-08-06 15:34:24 +0200

    Scripts for creating a planet with location on ways.

---
+++ master/run-update-low-planet.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+#
+#  run-update-low-planet-from-planet.sh
+#
+
+if [ "$USER" != "robot" ]; then
+    echo "Must be run as user robot"
+    exit 1
+fi
+
+set -e
+
+SERVER=update-low-planet
+
+# ccx41: 16 CPUs, 64 GB RAM, 360 GB disk
+STYPE=ccx41
+
+hcloud server create \
+    --name $SERVER \
+    --location nbg1 \
+    --type $STYPE \
+    --image debian-10 \
+    --ssh-key admin \
+    --user-data-from-file ~/osmdata/servers/$SERVER.yml \
+    --user-data-from-file ~/users.yml \
+    --user-data-from-file ~/ssh/keys.yml \
+    --volume planet
+
+IP=`hcloud server describe -o 'format={{.PublicNet.IPv4.IP}}' $SERVER`
+
+echo $IP
+
+sed -e "s/^IP /${IP} /" ~/ssh/known_hosts >~/.ssh/known_hosts
+
+echo "Waiting for system to become ready..."
+sleep 60
+ssh -o ConnectTimeout=300 robot@${IP} cloud-init status --wait
+echo "System initialized."
+
+ssh robot@${IP} mkdir low-planet
+scp ~/osmdata/scripts/low-planet/* robot@${IP}:low-planet/
+
+ssh robot@${IP} low-planet/update.sh
+ssh robot@${IP} sudo umount /mnt
+
+hcloud volume detach planet
+
+hcloud server delete $SERVER
+
+echo "run-update-planet done."
+
---
+++ scripts/low-planet/update.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+#------------------------------------------------------------------------------
+#
+#  low-planet/update.sh
+#
+#------------------------------------------------------------------------------
+
+iso_date='+%Y-%m-%dT%H:%M:%S'
+
+set -x
+set -e
+
+DATADIR=/mnt/data/planet
+
+PLANET=$DATADIR/planet.osm.pbf
+LOW_PLANET=$DATADIR/low-planet.osm.pbf
+
+NEW_LOW_PLANET=$DATADIR/new-low-planet.osm.pbf
+
+STARTTIME=`date $iso_date`
+
+mkdir -p $DATADIR
+
+date $iso_date
+
+echo "Removing leftover partial low planet (if any)..."
+rm -f $NEW_LOW_PLANET
+
+echo "Removing existing low planet (if any)..."
+rm -f $LOW_PLANET
+
+echo "Creating low planet from planet..."
+osmium add-locations-to-ways --keep-untagged-nodes -o $NEW_LOW_PLANET --fsync $PLANET && mv $NEW_LOW_PLANET $LOW_PLANET
+
+date $iso_date
+
+df -h
+
